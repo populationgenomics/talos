94e191ce629f721192a91acef753b50226eb7074
MattWellie <mattwellie@gmail.com>
2022-05-12 14:06:52 +1000

    can't drop a sub-field

--- reanalysis/vep/query.py
+++ reanalysis/vep/query.py
@@ -86,7 +86,4 @@ def vep_json_to_ht(vep_results_paths, out_path):
     ht = ht.annotate(locus=hl.locus(chrom, start))
     ht = ht.key_by(ht.locus)
 
-    # drop the huge input field?
-    ht = ht.drop(ht.vep.input)
-
     ht.write(str(out_path), overwrite=True)
