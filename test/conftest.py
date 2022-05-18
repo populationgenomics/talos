a3eb5ac54cc1193534dece71f6b129a526fa2e26
MattWellie <mattwellie@gmail.com>
2022-05-18 16:57:05 +1000

    migrate fixtures into conftest

--- test/conftest.py
+++ test/conftest.py
@@ -10,6 +10,7 @@ import hail as hl
 from hail.utils.java import FatalError
 
 from cyvcf2 import VCFReader
+from peddy.peddy import Ped
 
 from reanalysis.utils import AbstractVariant
 
@@ -24,6 +25,47 @@ DE_NOVO_TRIO = os.path.join(INPUT, 'de_novo.vcf.bgz')
 DE_NOVO_PED = os.path.join(INPUT, 'de_novo_ped.fam')
 LABELLED = os.path.join(INPUT, '1_labelled_variant.vcf.bgz')
 TEST_CONF = os.path.join(INPUT, 'test_conf.json')
+PED_FILE = os.path.join(INPUT, 'pedfile.ped')
+
+
+@pytest.fixture(name='peddy_ped', scope='session')
+def fixture_peddy_ped() -> Ped:
+    """
+
+    :return: Ped
+    """
+    return Ped(PED_FILE)
+
+
+@pytest.fixture(name='cleanup', scope='session', autouse=True)
+def fixture_hail_cleanup():
+    """
+    a fixture to clean up hail log files
+    irrelevant in CI, a right pain for local testing
+    auto-use + session + immediate yield means this is the last method call
+    :return:
+    """
+
+    # start hail once for the whole runtime
+    try:
+        hl.init(default_reference='GRCh38')
+    except FatalError:
+        print('failure - hail already initiated')
+
+    # yield something to suspend
+    yield ''
+
+    parent_dir = os.path.join(PWD, os.pardir)
+
+    log_files = [
+        filename
+        for filename in os.listdir(parent_dir)
+        if filename.startswith('hail') and os.path.splitext(filename)[1] == '.log'
+    ]
+
+    # remove all hail log files
+    for filename in log_files:
+        os.remove(os.path.join(parent_dir, filename))
 
 
 @pytest.fixture(name='hail_matrix')
@@ -78,34 +120,3 @@ def fixture_hail_matrix_comp_het():
     :return:
     """
     return hl.import_vcf(HAIL_MULTI_SAM, reference_genome='GRCh38')
-
-
-@pytest.fixture(name='cleanup', scope='session', autouse=True)
-def fixture_hail_cleanup():
-    """
-    a fixture to clean up hail log files
-    irrelevant in CI, a right pain for local testing
-    auto-use + session + immediate yield means this is the last method call
-    :return:
-    """
-
-    # start hail once for the whole runtime
-    try:
-        hl.init(default_reference='GRCh38')
-    except FatalError:
-        print('failure - hail already initiated')
-
-    # yield something to suspend
-    yield ''
-
-    parent_dir = os.path.join(PWD, os.pardir)
-
-    log_files = [
-        filename
-        for filename in os.listdir(parent_dir)
-        if filename.startswith('hail') and os.path.splitext(filename)[1] == '.log'
-    ]
-
-    # remove all hail log files
-    for filename in log_files:
-        os.remove(os.path.join(parent_dir, filename))
