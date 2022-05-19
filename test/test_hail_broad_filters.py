3e6f0e461fb0bcd21b89fd55c7a0f675f9599c7a
MattWellie <mattwellie@gmail.com>
2022-05-19 18:07:49 +1000

    corrected test

--- test/test_hail_broad_filters.py
+++ test/test_hail_broad_filters.py
@@ -49,26 +49,21 @@ def test_filter_matrix_by_ac_large():
 
 
 @pytest.mark.parametrize(
-    'filters,alleles,as_filt,length',
+    'filters,alleles,length',
     [
-        (hl.empty_set(hl.tstr), hl.literal(['A', 'C']), '', 1),
-        (hl.empty_set(hl.tstr), hl.literal(['A', '*']), '', 0),
-        (hl.empty_set(hl.tstr), hl.literal(['A', 'C', 'G']), '', 0),
-        (hl.literal({'fail'}), hl.literal(['A', 'C']), '', 0),
-        (hl.literal({'VQSR'}), hl.literal(['A', 'C']), 'PASS', 1),
+        (hl.empty_set(hl.tstr), hl.literal(['A', 'C']), 1),
+        (hl.empty_set(hl.tstr), hl.literal(['A', '*']), 0),
+        (hl.empty_set(hl.tstr), hl.literal(['A', 'C', 'G']), 0),
+        (hl.literal({'fail'}), hl.literal(['A', 'C']), 0),
+        (hl.literal({'VQSR'}), hl.literal(['A', 'C']), 0),
     ],
 )
-def test_filter_matrix_by_variant_attributes(
-    filters, alleles, as_filt, length, hail_matrix
-):
+def test_filter_matrix_by_variant_attributes(filters, alleles, length, hail_matrix):
     """
-    input 'values' are filters, alleles, and the as_fs annotation
-    - this AS_FilterStatus attribute is a result from VQSR application
-    - accepting filter=VQSR and AS_FS=PASS
+    input 'values' are filters, & alleles
 
     :param filters:
     :param alleles:
-    :param as_filt:
     :param length:
     :param hail_matrix:
     :return:
@@ -78,7 +73,6 @@ def test_filter_matrix_by_variant_attributes(
     anno_matrix = hail_matrix.annotate_rows(
         filters=filters,
         alleles=alleles,
-        info=hail_matrix.info.annotate(AS_FilterStatus=as_filt),
     )
 
     anno_matrix = filter_matrix_by_variant_attributes(anno_matrix)
