50e92d4aceca99ecc0a4e949dcf84151c96134e4
MattWellie <mattwellie@gmail.com>
2022-10-11 15:08:01 +1100

    update to example runner script

--- reanalysis/run_reanalysis.sh
+++ reanalysis/run_reanalysis.sh
@@ -3,15 +3,16 @@
 set -ex
 
 # set the date, or provide a default
-PAP_DATE=${1:-"2011-11-11"}
+PAP_DATE=${1:-$(date +%F)}
 
 analysis-runner \
+  --config reanalysis/reanalysis_global.toml \
+  --config reanalysis/reanalysis_cohort.toml \
   --dataset acute-care \
-  --description "AIP runtime test" \
+  --description "AIP run" \
   -o "reanalysis/${PAP_DATE}" \
   --access-level test \
   reanalysis/interpretation_runner.py \
-    --config_json gs://cpg-acute-care-test/reanalysis/reanalysis_conf.json \
     --input_path gs://cpg-acute-care-test/reanalysis/2011-11-11/prior_to_annotation.vcf.bgz \
     --panel_genes gs://cpg-acute-care-test/reanalysis/pre_panelapp_mendeliome.json \
     --plink_file gs://cpg-acute-care-test/reanalysis/acute-care-plink.fam
