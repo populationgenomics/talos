f72f77776756eb5e7433e226b5ef4a07f2260796
MattWellie <mattwellie@gmail.com>
2022-11-21 12:22:05 +1100

    county_county is a bad name

--- clinvar/conflict_huntr.py
+++ clinvar/conflict_huntr.py
@@ -203,9 +203,13 @@ def lines_from_gzip(filename: str) -> str:
             yield line.rstrip().split('\t')
 
 
-def county_county(subs: list[Submission]) -> Consequence:
+def consequence_decision(subs: list[Submission]) -> Consequence:
     """
-    count different consequence assignments
+    determine overall consequence assignment based on submissions
+    Args:
+        subs (): a list of submission objects for this allele
+    Returns:
+        a single Consequence object
     """
     # pylint: disable=R0912
 
@@ -312,7 +316,6 @@ def dict_list_to_ht(list_of_dicts: list) -> hl.Table:
 
     Returns:
         Hail table of the same content, indexed on locus & alleles
-
     """
 
     # convert list of dictionaries to a DataFrame
@@ -337,7 +340,7 @@ def get_all_decisions(
         allele_ids ():
 
     Returns:
-
+        dictionary of all alleles and their corresponding submissions
     """
     submission_dict = defaultdict(list)
 
@@ -462,7 +465,7 @@ def main(
         submissions = acmg_filter_submissions(submissions)
 
         # obtain an aggregate rating
-        rating = county_county(submissions)
+        rating = consequence_decision(submissions)
 
         # assess stars in remaining entries
         stars = check_stars(submissions)
