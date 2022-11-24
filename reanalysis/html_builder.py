264f0f9309f1d0b9c722707f09201c359721801a
MattWellie <mattwellie@gmail.com>
2022-11-25 10:16:24 +1100

    read categories directly

--- reanalysis/html_builder.py
+++ reanalysis/html_builder.py
@@ -54,27 +54,6 @@ def make_coord_string(var_coord: dict[str, str]) -> str:
     )
 
 
-def category_strings(var_data: dict[str, Any], sample: str) -> list[str]:
-    """
-    get a list of strings representing the categories present on this variant
-    :param var_data:
-    :param sample:
-    :return:
-    """
-    strings = [
-        cat.replace('categoryboolean', '')
-        for cat in var_data['info'].keys()
-        if cat.startswith('categoryboolean') and var_data['info'][cat]
-    ]
-    if var_data['info'].get('categorysupport'):
-        strings.append('support')
-
-    if sample in var_data['info'].get('categorysample4', []):
-        strings.append('de_novo')
-
-    return strings
-
-
 def color_csq(all_csq: set[str], mane_csq: set[str]) -> str:
     """
     takes the collection of all consequences, and MANE csqs
@@ -242,9 +221,7 @@ class HTMLBuilder:
 
                 # find all categories associated with this variant
                 # for each category, add to corresponding list and set
-                for category_value in category_strings(
-                    variant['var_data'], sample=sample
-                ):
+                for category_value in variant['var_data'].get('categories'):
                     if category_value == 'support':
                         continue
                     sample_variants[category_value].add(var_string)
@@ -377,11 +354,6 @@ class HTMLBuilder:
                 # pull out the string representation
                 var_string = make_coord_string(variant['var_data']['coords'])
 
-                # find list of all categories assigned
-                variant_categories = category_strings(
-                    variant['var_data'], sample=sample
-                )
-
                 csq_string, mane_string = get_csq_details(variant)
                 candidate_dictionaries.setdefault(variant['sample'], []).append(
                     {
@@ -395,7 +367,7 @@ class HTMLBuilder:
                                     lambda x: COLOR_STRING.format(
                                         color=COLORS[x], content=x
                                     ),
-                                    variant_categories,
+                                    variant['var_data']['categories'],
                                 )
                             )
                         ),
