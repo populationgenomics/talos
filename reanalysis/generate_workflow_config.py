171892884a704171239e5818f6318ec6f67b6e65
Greg Smith <gregsmi@microsoft.com>
2022-11-28 11:55:18 -0800

    run direct script

--- reanalysis/generate_workflow_config.py
+++ reanalysis/generate_workflow_config.py
@@ -15,14 +15,15 @@ python3 generate_workflow_config.py \
     --driver_image image \
     --output_prefix gregsmi \
     --extra_datasets severalgenomes rgp \
-    --deploy_config ~/sources/cpg/analysis-runner/deploy-config.prod.json
+    --deploy_config ~/sources/cpg/analysis-runner/deploy-config.prod.json \
+    --extra_configs reanalysis_global.toml reanalysis_cohort.toml
 """
 
 from argparse import ArgumentParser
 import json
 
 import toml
-from cpg_utils.config import update_dict
+from cpg_utils.config import read_configs, update_dict
 from cpg_utils.deploy_config import DeployConfig, get_deploy_config, get_workflow_config, set_deploy_config
 from cpg_utils.storage import get_dataset_bucket_config, get_dataset_bucket_url
 from cpg_utils import to_path
@@ -31,13 +32,13 @@ from cpg_utils import to_path
 parser = ArgumentParser()
 parser.add_argument("--dataset", help="name of primary dataset to target", required=True)
 parser.add_argument("--access_level", help="whether to target test or main buckets", required=True)
-parser.add_argument("--driver_image", help="Hail driverimage name (relative to main deployment CR) to target")
-parser.add_argument("--output_prefix", help="subpath on dataset's hail bucket to use for output", required=True)
+parser.add_argument("--driver_image", help="fully-qualified Hail driver image name to target", required=True)
+parser.add_argument("--output_prefix", help="subpath on dataset's access-level bucket to use for output", required=True)
 parser.add_argument("--extra_datasets", help="additional datasets to add storage url blocks for", nargs="*")
 parser.add_argument("--extra_configs", help="additional workflow-specific toml files to merge in", nargs="*")
 parser.add_argument("--deploy_config", help="deployment configuration file to use during generation")
 parser.add_argument("--print_only", help="print to screen, don't write to file", action="store_true")
-parser.add_argument("-o", help="output file name")
+parser.add_argument("-o", help="output file path and name (cloudpathlib.AnyPath-compatible paths are supported)")
 args = parser.parse_args()
 
 if args.deploy_config:
@@ -51,7 +52,8 @@ print(json.dumps(get_deploy_config().to_dict(), indent=2))
 workflow_config = get_workflow_config(args.dataset, args.access_level, args.driver_image, args.output_prefix)
 
 hail_config = {
-    "bucket": get_dataset_bucket_url(args.dataset, "hail")
+    "bucket": get_dataset_bucket_url(args.dataset, "hail"),
+    "billing_project": args.dataset
 }
 
 storage_config = { 
@@ -67,13 +69,14 @@ config = {
 }
 
 if args.extra_configs:
-    for extra_config in args.extra_configs:
-        update_dict(config, toml.load(extra_config))
+    extra_config = read_configs(args.extra_configs)
+    update_dict(config, extra_config)
 
 if args.print_only:
     print(toml.dumps(config))
 else:
     output_name = args.o or f"{args.dataset}_{args.access_level}.toml"
+    print(f"writing config toml to {output_name}")
     config_path = to_path(output_name)
     with config_path.open('w') as f:
         toml.dump(config, f)
---
+++ reanalysis/run_reanalysis_direct.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# set the date, or provide a default
+DATE=${1:-$(date +%F)}
+# make a randomized config name
+CONFIG_PATH=hail-az://sevgen002sa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
+
+python3 generate_workflow_config.py \
+  --dataset severalgenomes \
+  --access_level test \
+  --driver_image azcpg001acr.azurecr.io/cpg-common/images/hail-driver:latest \
+  --output_prefix "reanalysis/${DATE}" \
+  --extra_datasets severalgenomes rgp \
+  --extra_configs reanalysis_global.toml reanalysis_cohort.toml \
+  -o ${CONFIG_PATH}
+
+export CPG_CONFIG_PATH=${CONFIG_PATH}
+python3 interpretation_runner.py \
+  -i hail-az://sevgen002sa/test/reanalysis/986d792a448c66a8a5cfba65434e7d1ce9b1ff_1051-validation.mt \
+  --pedigree hail-az://sevgen002sa/test/reanalysis/pedigree.fam \
+  --skip_annotation
