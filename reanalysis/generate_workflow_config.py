53b8d3dae512c1c9bd6ad373e28f0256834e63ed
Greg Smith <gregsmi@microsoft.com>
2022-12-15 13:13:51 -0800

    update to include default refs and images

--- reanalysis/generate_workflow_config.py
+++ reanalysis/generate_workflow_config.py
@@ -5,9 +5,12 @@ Script for generating a properly-formatted basic workflow TOML
 file suitable for use with cpg-utils convenience functions 
 (dataset_path(), remote_tmpdir(), etc.)
 
-Requires CPG_DEPLOY_CONFIG environment variable to be set to an
+Requires the CPG_DEPLOY_CONFIG environment variable to be set to an
 appropriate Json deployment config, or a pointer to a Json file
-with the appropriate deployment config.
+with the appropriate deployment config. Requires any referenced 
+dataset to have a project mapping either in the server-config secret
+of a full live CPG analysis-runner deployment or in the passed-in
+server_config json argument.
 
 python3 generate_workflow_config.py \
     --dataset severalgenomes \
@@ -17,18 +20,63 @@ python3 generate_workflow_config.py \
     --extra_datasets severalgenomes rgp \
     --deploy_config ~/sources/cpg/cpg-deploy/azure/deploy-config.prod.json \
     --server_config ~/sources/cpg/cpg-deploy/aip/terraform.tfvars.json \
+    --image_base azcpg001acr.azurecr.io/cpg-common/images \
+    --reference_base hail-az://azcpg001sa/reference \
     --extra_configs reanalysis_global.toml reanalysis_cohort.toml \
     --print_only
 """
 
-from argparse import ArgumentParser
+import importlib.util as imp_util
 import json
+import os
+from argparse import ArgumentParser
+from typing import Any, Dict
 
+import requests
 import toml
-from cpg_utils.config import read_configs, update_dict
-from cpg_utils.deploy_config import DeployConfig, get_deploy_config, set_deploy_config, set_server_config
-from cpg_utils.storage import get_dataset_bucket_config, get_dataset_bucket_url
 from cpg_utils import to_path
+from cpg_utils.config import read_configs, update_dict
+from cpg_utils.deploy_config import (DeployConfig, get_deploy_config,
+                                     set_deploy_config, set_server_config)
+from cpg_utils.storage import get_dataset_bucket_config, get_dataset_bucket_url
+
+IMAGES_FILE = "https://raw.githubusercontent.com/populationgenomics/images/main/images.toml"
+REFERENCES_FILE = "https://raw.githubusercontent.com/populationgenomics/references/main/references.py"
+
+
+def generate_image_section(image_base: str) -> Dict[str, str]:
+    """
+    Copied from https://github.com/populationgenomics/images/blob/main/.github/workflows/prep_config.py.
+    Generates a set of absolute image paths from a registry prefix, based on the set in images.toml.
+    """
+    r = requests.get(IMAGES_FILE)
+    d = toml.loads(r.text)
+    # Manually adding cpg_workflows image.
+    d["cpg_workflows"] = "cpg_workflows:latest"
+    d = {k: f'{image_base}/{k}:{v}' for k, v in d.items()}
+    return d
+
+
+def generate_reference_section(reference_base: str) -> Dict[str, Any]:
+    """
+    Partially copied from https://github.com/populationgenomics/references/blob/main/.github/workflows/prep_config.py.
+    Generates a set of absolute reference paths from a storage prefix, based on the set in references.py.
+    """
+    r = requests.get(REFERENCES_FILE)
+    spec = imp_util.spec_from_loader('references', loader=None, origin=REFERENCES_FILE)
+    references = imp_util.module_from_spec(spec)
+    os.environ["PROJECT"] = "dummy-project"
+    exec(r.content, references.__dict__)
+
+    d = {'genome_build': references.GENOME_BUILD}
+
+    for source in references.SOURCES:
+        dst_path = f"{reference_base}/{source.dst}"
+        if not source.files:
+            d[source.name] = dst_path
+        else:
+            d[source.name] = { k: f"{dst_path}/{suffix}" for k, suffix in source.files.items() }
+    return d
 
 
 parser = ArgumentParser()
@@ -37,9 +85,11 @@ parser.add_argument("--access_level", help="whether to target test or main bucke
 parser.add_argument("--driver_image", help="fully-qualified Hail driver image name to target", required=True)
 parser.add_argument("--output_prefix", help="subpath on dataset's access-level bucket to use for output", required=True)
 parser.add_argument("--extra_datasets", help="additional datasets to add storage url blocks for", nargs="*")
-parser.add_argument("--extra_configs", help="additional workflow-specific toml files to merge in", nargs="*")
 parser.add_argument("--deploy_config", help="deployment configuration file to use during generation")
 parser.add_argument("--server_config", help="deployment dataset configuration file to use during generation")
+parser.add_argument("--image_base", help="base container registry path to use in creating an [images] section")
+parser.add_argument("--reference_base", help="base storage path to use in creating a [references] section")
+parser.add_argument("--extra_configs", help="additional workflow-specific toml files to merge in", nargs="*")
 parser.add_argument("--print_only", help="print to screen, don't write to file", action="store_true")
 parser.add_argument("-o", help="output file path and name (cloudpathlib.AnyPath-compatible paths are supported)")
 args = parser.parse_args()
@@ -53,7 +103,7 @@ if args.server_config:
         set_server_config(json.load(config_file)["datasets"])
 
 assert args.access_level == "test" or args.access_level == "main"
-print("generating workflow toml... using deploy_config:")
+print("generating workflow toml using deploy_config:")
 print(json.dumps(get_deploy_config().to_dict(include_datasets=True), indent=2))
 
 workflow_config = {
@@ -77,8 +127,14 @@ config = {
     "hail": hail_config,
     "workflow": workflow_config,
     "storage": storage_config,
-    "CPG_DEPLOY_CONFIG": get_deploy_config().to_dict(include_datasets=True)
+    "CPG_DEPLOY_CONFIG": get_deploy_config().to_dict()
 }
+
+if args.image_base:
+    config["images"] = generate_image_section(args.image_base)
+
+if args.reference_base:
+    config["references"] = generate_reference_section(args.reference_base)
 
 if args.extra_configs:
     extra_config = read_configs(args.extra_configs)
--- reanalysis/run_reanalysis_direct.sh
+++ reanalysis/run_reanalysis_direct.sh
@@ -7,15 +7,19 @@ DATE=${1:-$(date +%F)}
 # make a randomized config name
 CONFIG_PATH=hail-az://sevgen002sa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
 
+  # --deploy_config ~/sources/cpg/cpg-deploy/azure/deploy-config.prod.json \
+  # --server_config ~/sources/cpg/cpg-deploy/aip/terraform.tfvars.json \
 python3 generate_workflow_config.py \
   --dataset severalgenomes \
   --access_level test \
   --driver_image azcpg001acr.azurecr.io/cpg-common/images/cpg_workflows:latest \
   --output_prefix "reanalysis/${DATE}" \
   --extra_datasets severalgenomes rgp \
+  --image_base azcpg001acr.azurecr.io/cpg-common/images \
+  --reference_base hail-az://azcpg001sa/reference \
   --extra_configs reanalysis_global.toml reanalysis_cohort.toml \
   -o ${CONFIG_PATH}
-
+  
 export CPG_CONFIG_PATH=${CONFIG_PATH}
 python3 interpretation_runner.py \
   -i hail-az://sevgen002sa/test/reanalysis/986d792a448c66a8a5cfba65434e7d1ce9b1ff_1051-validation.mt \
