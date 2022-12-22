c548f40a958bec43979e3a5e5c6bd929d670d6b2
Miah Wander <miah@microsoft.com>
2022-12-22 00:52:39 +0000

    config tweaks to get RGP vep going

--- reanalysis/run_reanalysis.sh
+++ reanalysis/run_reanalysis.sh
@@ -14,5 +14,5 @@ analysis-runner \
   -o "reanalysis/${DATE}" \
   --access-level test \
   reanalysis/interpretation_runner.py \
-    -i hail-az://raregen001sa/test/inputs/joint-called-vcf_20221114/RGP_subset_samples.vcf.bgz \
+    -i hail-az://raregen001sa/test/inputs/rgp/RGP_ssub_vsub.vcf.bgz \
     --pedigree hail-az://raregen001sa/test/inputs/joint-called-vcf_20221114/RGP_Cases_for_MSFT_AIP_v0_trial.xlsx.fam
