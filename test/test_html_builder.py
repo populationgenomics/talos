80407e308714f86de3e2c581528937287ed57129
Cas Simons <cas.simons@populationgenomics.org.au>
2023-01-02 21:10:55 +1300

    Remove defunct test

--- test/test_html_builder.py
+++ test/test_html_builder.py
@@ -4,12 +4,4 @@ tests for the HTML generation script
 Not a huge number, this is not intended as a permanent solution
 """
 
-from reanalysis.html_builder import COLORS
 
-
-def test_set_up_categories():
-    """
-
-    :return:
-    """
-    assert COLORS['de_novo'] == '#FF0000'
