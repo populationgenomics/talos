ef6abff2aaa6e79af6c195db75d0738aaa0cf4c0
Miah Wander <miah@microsoft.com>
2023-01-24 21:35:38 +0000

    cohort toml and runner changes for this workload

--- reanalysis/run_reanalysis_direct.sh
+++ reanalysis/run_reanalysis_direct.sh
@@ -5,7 +5,8 @@ set -ex
 # Make sure to export CPG_DEPLOY_CONFIG first?
 
 # set the date, or provide a default
-DATE=${1:-$(date +%F)}
+#DATE=${1:-$(date +%F)}
+DATE="2023-01-23"
 # make a randomized config name
 CONFIG_PATH=hail-az://sevgen002sa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 8).toml
 
@@ -14,7 +15,7 @@ CONFIG_PATH=hail-az://sevgen002sa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/
 python3 generate_workflow_config.py \
   --dataset rgp \
   --access_level test \
-  --driver_image azcpg001acr.azurecr.io/cpg-common/images/cpg_workflows:latest \
+  --driver_image azcpg001acr.azurecr.io/cpg-common/images/cpg_aip:latest \
   --output_prefix "reanalysis/${DATE}" \
   --extra_datasets severalgenomes rgp \
   --image_base azcpg001acr.azurecr.io/cpg-common/images \
@@ -24,5 +25,6 @@ python3 generate_workflow_config.py \
   
 export CPG_CONFIG_PATH=${CONFIG_PATH}
 python3 interpretation_runner.py \
-  -i hail-az://raregen001sa/test/inputs/joint-called-vcf_20221114/RGP_subset_samples.vcf.bgz \
-  --pedigree hail-az://raregen001sa/test/inputs/joint-called-vcf_20221114/RGP_Cases_for_MSFT_AIP_v0_trial.xlsx.fam
+  -i hail-az://raregen001sa/test/inputs/rgp/rgp_test.vcf.bgz \
+  --pedigree hail-az://raregen001sa/test/inputs/joint-called-vcf_20221114/RGP_Cases_for_MSFT_AIP_v0_trial.xlsx.fam \
+  --participant_panels hail-az://raregen001sa/test/inputs/rgp/rgp_party_panels.json
