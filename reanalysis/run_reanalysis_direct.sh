92c970ca9359f58b650f05dd27d7ead3da334221
Miah Wander <miah@microsoft.com>
2023-01-26 17:52:26 +0000

    direct runner now executes from repo root

--- reanalysis/run_reanalysis_direct.sh
+++ reanalysis/run_reanalysis_direct.sh
@@ -12,7 +12,7 @@ CONFIG_PATH=hail-az://sevgen002sa/test/config-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/
 
   # --deploy_config ~/sources/cpg/cpg-deploy/azure/deploy-config.prod.json \
   # --server_config ~/sources/cpg/cpg-deploy/aip/terraform.tfvars.json \
-python3 generate_workflow_config.py \
+python3 reanalysis/generate_workflow_config.py \
   --dataset rgp \
   --access_level test \
   --driver_image azcpg001acr.azurecr.io/cpg-common/images/cpg_aip:latest \
@@ -20,11 +20,11 @@ python3 generate_workflow_config.py \
   --extra_datasets severalgenomes rgp \
   --image_base azcpg001acr.azurecr.io/cpg-common/images \
   --reference_base hail-az://azcpg001sa/reference \
-  --extra_configs reanalysis_global.toml reanalysis_cohort.toml \
+  --extra_configs reanalysis/reanalysis_global.toml reanalysis/reanalysis_cohort.toml \
   -o ${CONFIG_PATH}
   
 export CPG_CONFIG_PATH=${CONFIG_PATH}
-python3 interpretation_runner.py \
+python3 reanalysis/interpretation_runner.py \
   -i hail-az://raregen001sa/test/inputs/rgp/rgp_train.vcf.bgz \
   --pedigree hail-az://raregen001sa/test/inputs/rgp/rgp_train.fam \
   --participant_panels hail-az://raregen001sa/test/inputs/rgp/rgp_default_panels.json
