13b954bc740f73131acc80a07bb7cbc7abfe26eb
MattWellie <mattwellie@gmail.com>
2023-02-11 08:49:23 +1100

    command correction

--- release_scripts/broad_rgp_release.sh
+++ release_scripts/broad_rgp_release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-gsutil -m -u broad-rgp cp -r \
+gcloud storage --project=broad-rgp cp --recursive \
     gs://cpg-broad-rgp-main/mt/3355c9263be6d4b6e13c88b95fb0e3bc1bc99d_1559-broad-rgp.mt \
-    gs://cpg-broad-rgp-main-release/mt/3355c9263be6d4b6e13c88b95fb0e3bc1bc99d_1559-broad-rgp.mt
+    gs://cpg-broad-rgp-release/3355c9263be6d4b6e13c88b95fb0e3bc1bc99d_1559-broad-rgp.mt
