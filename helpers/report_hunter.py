52ad34325649919fb795ea6847a3120bc5a1878e
MattWellie <mattwellie@gmail.com>
2023-02-27 14:47:32 +1100

    include a jinja template

--- helpers/report_hunter.py
+++ helpers/report_hunter.py
@@ -1,13 +1,20 @@
 """
 track down the latest version of all reports
+generate an index HTML page with links to all reports
 """
 
 
+from dataclasses import dataclass
 from os.path import join
+from pathlib import Path
+
+import jinja2
 from google.cloud import storage
+
 from cpg_utils import to_path
 
 
+JINJA_TEMPLATE_DIR = Path(__file__).absolute().parent / 'templates'
 TEMPLATE = 'gs://cpg-{dataset}-main-web/reanalysis'
 WEB_TEMPLATE = 'https://main-web.populationgenomics.org.au'
 
@@ -35,58 +42,68 @@ cohorts = [
     'udn-aus',
 ]
 
-all_cohorts = {}
 
-for cohort in cohorts:
-    all_cohorts[cohort] = {}
-    ROOT = TEMPLATE.format(dataset=cohort)
-    bucket_name, prefix = ROOT.removeprefix('gs://').split('/', maxsplit=1)
-    client = storage.Client()
-    bucket = client.bucket(bucket_name)
-    blobs = sorted(
-        client.list_blobs(bucket_name, prefix=prefix), key=lambda x: x.updated
+@dataclass
+class Report:
+    """
+    generic object for storing report details
+    """
+
+    dataset: str
+    address: str
+    genome_or_exome: str
+    subtype: str
+    date: str
+
+
+def main():
+    """
+    finds all existing reports, generates an HTML file
+    """
+
+    all_cohorts = {}
+
+    for cohort in cohorts:
+        root = TEMPLATE.format(dataset=cohort)
+        bucket_name, prefix = root.removeprefix('gs://').split('/', maxsplit=1)
+        client = storage.Client()
+        blobs = sorted(
+            client.list_blobs(bucket_name, prefix=prefix), key=lambda x: x.updated
+        )
+
+        # iterate through, oldest to latest, replacing as new ones are found
+        for blob in blobs:
+            if not blob.name.endswith('html'):
+                continue
+
+            report_obj = Report(
+                dataset=cohort,
+                address=join(WEB_TEMPLATE, cohort, blob.name),
+                genome_or_exome='Exome' if '/exomes/' in blob.name else 'Genome',
+                subtype='Singleton' if 'singleton_' in blob.name else 'Familial',
+                date=str(blob.updated).split()[0],
+            )
+            all_cohorts[
+                f'{cohort}_{report_obj.genome_or_exome}_{report_obj.subtype}'
+            ] = report_obj
+
+    # smoosh into a list for the report context - all reports sortable by date
+    template_context = {'reports': list(all_cohorts.values())}
+
+    # build some HTML
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(JINJA_TEMPLATE_DIR),
+    )
+    template = env.get_template('index.html.jinja')
+    content = template.render(**template_context)
+
+    # write this file to a web bucket - either attached to a single dataset, or communal
+    output_path = join(TEMPLATE.format(dataset='common'), 'aip_index.html')
+    output_path = output_path.replace('main', 'test')
+    to_path(output_path).write_text(
+        '\n'.join(line for line in content.split('\n') if line.strip())
     )
 
-    # iterate through, oldest to latest, replacing as new ones are found
-    for blob in blobs:
-        if not blob.name.endswith('html'):
-            continue
-        if 'singleton_' in blob.name:
-            if '/exomes/' in blob.name:
-                all_cohorts[cohort]['singleton_exome'] = join(
-                    WEB_TEMPLATE, cohort, blob.name
-                )
-            else:
-                all_cohorts[cohort]['singleton_genome'] = join(
-                    WEB_TEMPLATE, cohort, blob.name
-                )
-        else:
-            if '/exomes/' in blob.name:
-                all_cohorts[cohort]['exome'] = join(WEB_TEMPLATE, cohort, blob.name)
-            else:
-                all_cohorts[cohort]['genome'] = join(WEB_TEMPLATE, cohort, blob.name)
 
-HTML = """
-<!DOCTYPE html>
-
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Latest AIP Reports</title>
-    <meta name="description" content="Latest AIP Reports">
-  </head>
-  <body>
-"""
-
-for cohort, categories in all_cohorts.items():
-    HTML += f'\t<h2>{cohort}</h2><br>\n'
-    for cat in sorted(categories):
-        HTML += f'\t\t<a href="{categories[cat]}" target="_blank">{cat}</a><br>\n'
-HTML += '</body>'
-
-# write this file to a web bucket - either attached to a single dataset, or communal
-output_path = join(TEMPLATE.format(dataset='common'), 'aip_index.html')
-output_path = output_path.replace('main', 'test')
-
-with to_path(output_path).open('w') as handle:
-    handle.writelines(HTML)
+if __name__ == '__main__':
+    main()
