33b43d7833758d29e63228ab7dd7dbc3fce3700f
MattWellie <mattwellie@gmail.com>
2023-05-04 10:34:46 +1000

    Bump version: 1.0.1 → 1.1.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     name='automated-interpretation-pipeline',
     description='CPG Variant Prioritisation',
     long_description=readme,
-    version='1.0.1',
+    version='1.1.0',
     author='Matthew Welland, CPG',
     author_email=(
         'matthew.welland@populationgenomics.org.au, '
