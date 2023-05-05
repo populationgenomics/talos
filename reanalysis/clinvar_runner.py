304aedbf843f886481caeae0472262c304733e0f
MattWellie <mattwellie@gmail.com>
2023-05-05 11:05:43 +1000

    tweaks!

--- reanalysis/clinvar_runner.py
+++ reanalysis/clinvar_runner.py
@@ -8,7 +8,7 @@ Entrypoint for clinvar summary generation
 from datetime import datetime
 
 import click
-from cpg_utils import to_path
+from cpg_utils import to_path, Path
 from cpg_utils.config import get_config
 from cpg_utils.hail_batch import (
     authenticate_cloud_credentials_in_job,
@@ -21,23 +21,19 @@ from reanalysis import clinvar_by_codon, summarise_clinvar_entries, seqr_loader
 from reanalysis.vep_jobs import add_vep_jobs
 
 
-@click.command
-@click.option('--ht_out', help='Path to write the Hail table to')
-@click.option('--date', help='date cut-off, optional', default=None)
-def main(ht_out: str, date: str | None = None):
-    """
-    run the clinvar summary, output to defined path
-    Args:
-        ht_out ():
-        date ():
+def generate_clinvar_table(
+    clinvar_table_path: Path, snv_vcf: Path, date: str | None = None
+):
     """
 
-    clinvar_table_path = to_path(ht_out)
-    clinvar_folder = clinvar_table_path.parent
+    Returns:
 
-    # create a bash job to copy data from remote
+    """
+
     bash_job = get_batch().new_bash_job(name='copy clinvar files to local')
     bash_job.image(get_config()['workflow']['driver_image'])
+
+    clinvar_folder = clinvar_table_path.parent
 
     directory = 'https://ftp.ncbi.nlm.nih.gov/pub/clinvar/tab_delimited/'
     sub_file = 'submission_summary.txt.gz'
@@ -57,24 +53,46 @@ def main(ht_out: str, date: str | None = None):
     get_batch().write_output(bash_job.subs, str(clinvar_folder / f'{today}_{sub_file}'))
     get_batch().write_output(bash_job.vars, str(clinvar_folder / f'{today}_{var_file}'))
 
-    # create a space for the SNV VCF
-    snv_vcf = clinvar_folder / 'pathogenic_snv.vcf.bgz'
-
     # region: run the summarise_clinvar_entries script
     summarise = get_batch().new_job(name='summarise clinvar')
-    prior_job = summarise
+    summarise.depends_on(bash_job)
 
     summarise.cpu(2).image(get_config()['workflow']['driver_image']).storage('20G')
     authenticate_cloud_credentials_in_job(summarise)
     command_options = (
         f'-s {bash_job.subs} '
         f'-v {bash_job.vars} '
-        f'-o {ht_out} '
+        f'-o {clinvar_table_path} '
         f'--path_snv {snv_vcf} '
     )
     if date:
         command_options += f' -d {date}'
     summarise.command(f'python3 {summarise_clinvar_entries.__file__} {command_options}')
+
+    return summarise
+
+
+@click.command
+@click.option('--ht_out', help='Path to write the Hail table to')
+@click.option('--date', help='date cut-off, optional', default=None)
+def main(ht_out: str, date: str | None = None):
+    """
+    run the clinvar summary, output to defined path
+    Args:
+        ht_out ():
+        date ():
+    """
+
+    # region write ClinVar table and VCF
+    clinvar_table_path = to_path(ht_out)
+    clinvar_folder = clinvar_table_path.parent
+
+    # create a space for the SNV VCF
+    snv_vcf = clinvar_folder / 'pathogenic_snv.vcf.bgz'
+
+    dependency = None
+    if not all(output.exists() for output in [clinvar_table_path, snv_vcf]):
+        dependency = generate_clinvar_table(clinvar_table_path, snv_vcf, date)
     # endregion
 
     # region: annotate the SNV VCF with VEP
@@ -91,10 +109,11 @@ def main(ht_out: str, date: str | None = None):
     )
 
     # add Clinvar job as an annotation dependency
-    if prior_job and vep_jobs:
+    if dependency and vep_jobs:
         for job in vep_jobs:
-            job.depends_on(prior_job)
-        prior_job = vep_jobs[-1]
+            job.depends_on(dependency)
+    if vep_jobs:
+        dependency = vep_jobs[-1]
 
     j = get_batch().new_job(f'annotate cohort')
     j.image(get_config()['workflow']['driver_image'])
@@ -109,9 +128,9 @@ def main(ht_out: str, date: str | None = None):
             setup_gcp=True,
         )
     )
-    if prior_job:
-        j.depends_on(prior_job)
-    prior_job = j
+    if dependency:
+        j.depends_on(dependency)
+    dependency = j
     # endregion
 
     # region: run the clinvar_by_codon script
@@ -127,7 +146,7 @@ def main(ht_out: str, date: str | None = None):
         f'--mt {annotated_clinvar} '
         f'--write_path {pm5_table}'
     )
-    clinvar_by_codon_job.depends_on(prior_job)
+    clinvar_by_codon_job.depends_on(dependency)
     # endregion
     get_batch().run(wait=False)
 
--- reanalysis/seqr_loader.py
+++ reanalysis/seqr_loader.py
@@ -10,41 +10,58 @@ import hail as hl
 from cpg_utils.hail_batch import reference_path, genome_build
 
 
-def annotate_cohort(
-    vcf_path,
-    out_mt_path,
-    vep_ht_path,
-    checkpoint_prefix=None,
-):
+def annotate_cohort(vcf_path, out_mt_path, vep_ht_path, checkpoint_prefix=None):
     """
     Convert VCF to matrix table, annotate for Seqr Loader, add VEP and VQSR
     annotations.
     """
 
+    # set up a logger in this Hail Query runtime
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    def _read(path):
+        if path.strip('/').endswith('.ht'):
+            t = hl.read_table(str(path))
+        else:
+            assert path.strip('/').endswith('.mt')
+            t = hl.read_matrix_table(str(path))
+        logging.info(f'Read checkpoint {path}')
+        return t
+
+    def _checkpoint(t, file_name):
+        if checkpoint_prefix:
+            path = os.path.join(checkpoint_prefix, file_name)
+            logging.info(f'Checkpointing to {path}')
+
+            t.write(str(path), overwrite=True)
+            logging.info(f'Wrote checkpoint {path}')
+            t = _read(str(path))
+        return t
+
+    logger.info(f'Importing VCF {vcf_path}')
     mt = hl.import_vcf(
         str(vcf_path),
         reference_genome=genome_build(),
         skip_invalid_loci=True,
         force_bgz=True,
     )
-    logging.info(f'Importing VCF {vcf_path}')
-    if checkpoint_prefix:
-        mt = mt.checkpoint(
-            os.path.join(checkpoint_prefix, 'mt-from-vcf.mt'), overwrite=True
-        )
 
-    logging.info(f'Loading VEP Table from {vep_ht_path}')
-    vep_ht = hl.read_table(vep_ht_path)
-    logging.info(f'Adding VEP annotations into the Matrix Table from {vep_ht_path}')
+    logger.info(f'Loading VEP Table from {vep_ht_path}')
+    vep_ht = _read(vep_ht_path)
+
+    logger.info(f'Adding VEP annotations into the Matrix Table from {vep_ht_path}')
     mt = mt.annotate_rows(vep=vep_ht[mt.locus, mt.alleles].vep)
+
+    mt = _checkpoint(mt, 'mt-plus-vep.mt')
 
     # Add potentially missing fields
     if not all(attr in mt.row_value for attr in ['AC', 'AF', 'AN']):
         if mt.count_cols() == 0:
-            logging.info('No samples in the Matrix Table, adding dummy values')
+            logger.info('No samples in the Matrix Table, adding dummy values')
             mt = mt.annotate_rows(AN=1, AF=0.01, AC=1)
         else:
-            logging.info('Adding AC/AF/AN attributes from variant_qc')
+            logger.info('Adding AC/AF/AN attributes from variant_qc')
             mt = hl.variant_qc(mt)
             mt = mt.annotate_rows(
                 AN=mt.variant_qc.AN, AF=mt.variant_qc.AF[1], AC=mt.variant_qc.AC[1]
@@ -56,10 +73,12 @@ def annotate_cohort(
         if isinstance(mt[attr], hl.ArrayExpression):
             mt = mt.annotate_rows(**{attr: mt[attr][1]})
 
-    logging.info('Annotating with seqr-loader aggregate data')
-    ref_ht = hl.read_table(str(reference_path('seqr_combined_reference_data')))
-    clinvar_ht = hl.read_table(str(reference_path('seqr_clinvar')))
+    logger.info('Annotating with seqr-loader aggregate data')
+    ref_ht = _read(str(reference_path('seqr_combined_reference_data')))
+    clinvar_ht = _read(str(reference_path('seqr_clinvar')))
     mt = mt.annotate_rows(clinvar_data=clinvar_ht[mt.row_key], **ref_ht[mt.row_key])
+
+    mt = _checkpoint(mt, 'mt-plus-ref-data.mt')
 
     mt = mt.annotate_rows(
         geneIds=hl.set(mt.vep.transcript_consequences.map(lambda c: c.gene_id)),
@@ -81,4 +100,4 @@ def annotate_cohort(
     mt.describe()
     mt.write(str(out_mt_path), overwrite=True)
 
-    logging.info(f'Written final matrix table into {out_mt_path}')
+    logger.info(f'Written final matrix table into {out_mt_path}')
