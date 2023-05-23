57b7355fb6c82ff970d9b42eae9009a2e155b7cc
Matt Welland <mattwellie@gmail.com>
2023-05-23 19:05:33 +1000

    Release Clinvar data (#301)

--- release_scripts/csiro_release.sh
+++ release_scripts/csiro_release.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-# https://github.com/populationgenomics/seqr-private/issues/11
 
-# copy single files
-gsutil -m -u acute-care-321904 cp \
-    gs://cpg-acute-care-test/csiro_copy/callset.vcf.bgz \
-    gs://cpg-acute-care-test/csiro_copy/hpo_terms.json \
-    gs://cpg-acute-care-test/csiro_copy/hpo_terms.tsv \
-    gs://cpg-acute-care-test/csiro_copy/pedigree.fam \
-    gs://cpg-acute-care-release/csiro
+# copy ClinVar Data
+gsutil -m -u acute-care-321904 cp -r \
+    gs://cpg-acute-care-test/csiro
+    gs://cpg-acute-care-release/csiro_clinvar_23_05_2023
