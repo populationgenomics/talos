30930a7d61c9519466a0083135908f995cd04ae8
MattWellie <mattwellie@gmail.com>
2023-11-03 16:17:06 +1100

    Correction to logic parentheses

--- reanalysis/interpretation_runner.py
+++ reanalysis/interpretation_runner.py
@@ -404,7 +404,7 @@ def main(
             ANNOTATED_MT = input_path
 
         else:
-            if not to_path(INPUT_AS_VCF).exists() and to_path(SITES_ONLY).exists():
+            if not (to_path(INPUT_AS_VCF).exists() and to_path(SITES_ONLY).exists()):
                 prior_job = setup_mt_to_vcf(input_file=input_path)
 
             # overwrite input path with file we just created
