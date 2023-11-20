8893c1a10243e4682b5642531baf41556ab9a412
MattWellie <mattwellie@gmail.com>
2023-11-20 13:44:00 +1100

    Only generate hyperlinks for seqr families

--- reanalysis/html_builder.py
+++ reanalysis/html_builder.py
@@ -371,7 +371,7 @@ class Sample:
         self.ext_id = metadata['ext_id']
         self.panel_ids = metadata['panel_ids']
         self.panel_names = metadata['panel_names']
-        self.seqr_id = html_builder.seqr.get(name, name)
+        self.seqr_id = html_builder.seqr.get(name, None)
         self.ext_labels = ext_labels
         self.html_builder = html_builder
 
