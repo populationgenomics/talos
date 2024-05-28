b50ac1826f9daa322ad7d973dafd037e89dba4f5
MattWellie <mattwellie@gmail.com>
2024-05-28 11:59:29 +1000

    fixed

--- reanalysis/cpg_generate_pheno_ped.py
+++ reanalysis/cpg_generate_pheno_ped.py
@@ -62,16 +62,15 @@ def get_data_from_metamist(project: str) -> list[list[str]]:
     # first get a lookup of Int IDs to Ext IDs
     result = query(PARTICIPANT_QUERY, variables={'project': project})
 
-    # will map CPG IDs to external project participant IDs
-    int_to_ext: dict[str, str] = {}
+    # maps External IDs from the Pedigree endpoint to Internal CPG IDs
+    ext_to_int: dict[str, str] = {}
 
     # will map each CPG ID to its set of HPO terms (can be empty)
     cpg_to_hpos: dict[str, set[str]] = {}
 
     # iterate over all SG Entities
     for sg in result['project']['sequencingGroups']:
-
-        int_to_ext[sg['id']] = sg['sample']['participant']['externalId']
+        ext_to_int[sg['sample']['participant']['externalId']] = sg['id']
         cpg_to_hpos[sg['id']] = set(HPO_RE.findall(sg['sample']['participant']['phenotypes'].get(HPO_KEY, '')))
 
     # iterate over the pedigree entities, forming a list from each. List elements:
@@ -79,16 +78,17 @@ def get_data_from_metamist(project: str) -> list[list[str]]:
     for entry in result['project']['pedigree']:
         ped_row: list[str] = [
             entry['family_id'],
-            entry['individual_id'],
-            entry['paternal_id'] or '0',
-            entry['maternal_id'] or '0',
+            ext_to_int.get(entry['individual_id'], entry['individual_id']),  # defaults to internal ID
+            ext_to_int.get(entry['paternal_id'], entry['paternal_id']) or '0',
+            ext_to_int.get(entry['maternal_id'], entry['maternal_id']) or '0',
             str(entry['sex']),
             str(entry['affected']),
-            int_to_ext.get(entry['individual_id'], entry['individual_id']),  # defaults to internal ID again
+            entry['individual_id'],
         ]
 
         # if there are recorded HPOs, extend the row with them
-        if hpos := cpg_to_hpos.get(entry['individual_id']):
+        assert isinstance(entry['individual_id'], str)
+        if hpos := cpg_to_hpos.get(ext_to_int.get(entry['individual_id'], 'missing')):
             ped_row.extend(sorted(hpos))
 
         ped_entries.append(ped_row)
