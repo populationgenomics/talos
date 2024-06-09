eeee2d32eee7be775912bf3dde41b06b20c5e625
MattWellie <mattwellie@gmail.com>
2024-06-09 20:03:33 +1000

    Bump version: 3.4.0 → 4.0.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     name='Talos',
     description='CPG Variant Prioritisation',
     long_description=readme,
-    version='3.4.0',
+    version='4.0.0',
     author='Matthew Welland, CPG',
     author_email='matthew.welland@populationgenomics.org.au, cas.simons@populationgenomics.org.au',
     package_data={'talos': ['templates/*.jinja', 'reanalysis_global.toml']},
