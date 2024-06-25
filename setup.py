67515d811075a777950446cf4464fb38844b6a72
MattWellie <mattwellie@gmail.com>
2024-06-26 09:08:56 +1000

    remove docker build, correction to the report_hunter path

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', encoding='utf-8') as handle:
 def read_reqs(filename: str) -> list[str]:
     """
     Read requirements from a file, return as a list
-
+    TODO eventually split out the requirements vs. the setup content
 
     Args:
         filename (str): the requirements file to parse
@@ -54,14 +54,23 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'generate_pedigree = talos.cpg_generate_pheno_ped:main',
+            # for use in translating a VEP annotated VCF to a MatrixTable
             'vcf_to_mt = talos.vep_vcf_to_mt:main',
+            # CPG internal, scans database for published reports, collects into an index page
             'report_hunter = helpers.report_hunter:run_both',
-            'hail_label = talos.hail_filter_and_label:cli_main',
-            'hail_label_sv = talos.hail_filter_sv:cli_main',
+            # CPG implementation, builds an extended Pedigree format
+            'generate_pedigree = talos.cpg_generate_pheno_ped:main',
+            # use the HPO terms to select panels for this analysis
             'hpo_panel_match = talos.hpo_panel_match:cli_main',
+            # query PanelApp for those selected panels
             'query_panelapp = talos.query_panelapp:cli_main',
+            # Filter and label a small-variant MatrixTable
+            'hail_label = talos.hail_filter_and_label:cli_main',
+            # Filter and label a SV MatrixTable
+            'hail_label_sv = talos.hail_filter_sv:cli_main',
+            # Run each of the category-labelled variants through MOI filters
             'validate_categories = talos.validate_categories:cli_main',
+            # CPG internal (?), publish those results as an HTML report
             'build_html = talos.html_builder:main',
         ]
     },
