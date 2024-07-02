55c9552d4b12e7fc842b2a78cf376eb7fb3c86eb
MattWellie <mattwellie@gmail.com>
2024-07-02 17:21:18 +1000

    use the right entrypoint

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
             # Run each of the category-labelled variants through MOI filters
             'ValidateMOI = talos.ValidateMOI:cli_main',
             # CPG internal (?), publish those results as an HTML report
-            'CreateTalosHTML = talos.CreateTalosHTML:main',
+            'CreateTalosHTML = talos.CreateTalosHTML:cli_main',
             # CPG internal (?), generate a file for ingestion by Seqr
             'GenerateSeqrFile = talos.minimise_output_for_seqr:cli_main',
         ]
