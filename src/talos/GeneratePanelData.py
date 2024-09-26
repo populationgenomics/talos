3be989a03ad049fddaeb4f6dc7bc4a693584b18d
Matt Welland <mattwellie@gmail.com>
2024-09-26 14:57:38 +1000

    Feature 433: Remove HPO tree limit (#434)
    * remove a limit on the number of HPO layers scanned

--- src/talos/GeneratePanelData.py
+++ src/talos/GeneratePanelData.py
@@ -20,7 +20,6 @@ from talos.models import ParticipantHPOPanels, PhenoPacketHpo, PhenotypeMatchedP
 from talos.static_values import get_logger
 
 HPO_RE = re.compile(r'HP:[0-9]+')
-MAX_DEPTH = 3
 
 PANELAPP_HARD_CODED_DEFAULT = 'https://panelapp.agha.umccr.org/api/v1/panels'
 try:
@@ -117,7 +116,6 @@ def match_hpo_terms(
     panel_map: dict[str, set[int]],
     hpo_tree: nx.MultiDiGraph,
     hpo_str: str,
-    layers_scanned: int = 0,
     selections: set[int] | None = None,
 ) -> set[int]:
     """
@@ -132,7 +130,6 @@ def match_hpo_terms(
         panel_map (dict):
         hpo_tree (): a graph object representing the HPO tree
         hpo_str (str): the query HPO term
-        layers_scanned (int): number of layers traversed so far
         selections (set[int]): collected panel IDs so far
 
     Returns:
@@ -146,12 +143,6 @@ def match_hpo_terms(
     if hpo_str in panel_map:
         selections.update(panel_map[hpo_str])
 
-    # at time of writing the constant MAX_DEPTH is 3
-    # i.e. once the HPO tree traversal has reached 3 layers up, stop
-    # layers in this context represents a reduction in term specificity
-    if layers_scanned >= MAX_DEPTH:
-        return selections
-
     # if a node is invalid, recursively call this method for each replacement D:
     # there are simpler ways, just none that are as fun to write
     if not hpo_tree.has_node(hpo_str):
@@ -161,18 +152,18 @@ def match_hpo_terms(
     hpo_node = hpo_tree.nodes[hpo_str]
     if hpo_node.get('is_obsolete', 'false') == 'true':
         for hpo_term in hpo_node.get('replaced_by', []):
-            selections.update(match_hpo_terms(panel_map, hpo_tree, hpo_term, layers_scanned + 1, selections))
+            selections.update(match_hpo_terms(panel_map, hpo_tree, hpo_term, selections))
     # search for parent(s), even if the term is obsolete
     for hpo_term in hpo_node.get('is_a', []):
-        selections.update(match_hpo_terms(panel_map, hpo_tree, hpo_term, layers_scanned + 1, selections))
+        selections.update(match_hpo_terms(panel_map, hpo_tree, hpo_term, selections))
     return selections
 
 
-def match_hpos_to_panels(hpo_to_panel_map: dict, hpo_file: str, all_hpos: set[str]) -> tuple[dict, dict[str, str]]:
+def match_hpos_to_panels(hpo_panel_map: dict, hpo_file: str, all_hpos: set[str]) -> tuple[dict, dict[str, str]]:
     """
     take the HPO terms from the participant metadata, and match to panels
     Args:
-        hpo_to_panel_map (dict): panel IDs to all related panels
+        hpo_panel_map (dict): panel IDs to all related panels
         hpo_file (str): path to an obo file containing HPO tree
         all_hpos (set[str]): collection of all unique HPO terms
 
@@ -194,7 +185,7 @@ def match_hpos_to_panels(hpo_to_panel_map: dict, hpo_file: str, all_hpos: set[st
 
     hpo_to_panels = {}
     for hpo in all_hpos:
-        panel_ids = match_hpo_terms(panel_map=hpo_to_panel_map, hpo_tree=hpo_graph, hpo_str=hpo)
+        panel_ids = match_hpo_terms(panel_map=hpo_panel_map, hpo_tree=hpo_graph, hpo_str=hpo)
         hpo_to_panels[hpo] = panel_ids
 
     return hpo_to_panels, hpo_to_text
@@ -261,11 +252,7 @@ def main(ped_file: str, hpo_file: str, panel_out: str | None):
     """
     panels_by_hpo = get_panels()
     hpo_dict, all_hpo = get_participant_hpos(pedigree=ped_file)
-    hpo_to_panels, hpo_to_text = match_hpos_to_panels(
-        hpo_to_panel_map=panels_by_hpo,
-        hpo_file=hpo_file,
-        all_hpos=all_hpo,
-    )
+    hpo_to_panels, hpo_to_text = match_hpos_to_panels(hpo_panel_map=panels_by_hpo, hpo_file=hpo_file, all_hpos=all_hpo)
     match_participants_to_panels(hpo_dict, hpo_to_panels)
 
     # update the HPO terms to be {'id': 'HPO:#', 'label': 'Description'}
