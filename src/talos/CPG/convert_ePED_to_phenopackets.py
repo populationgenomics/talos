fa7cc729e471a2f538c4ff49851b17db755d7059
MattWellie <mattwellie@gmail.com>
2024-10-20 13:02:09 +1000

    ePED converter finished

--- src/talos/CPG/convert_ePED_to_phenopackets.py
+++ src/talos/CPG/convert_ePED_to_phenopackets.py
@@ -3,10 +3,12 @@ script for converting the 'extended PED format' to a regular PED file and Phenop
 """
 
 from argparse import ArgumentParser
-from peds import open_ped
+from os import makedirs
+from os.path import join, exists
 
 import phenopackets.schema.v2 as pps2
 from google.protobuf.json_format import MessageToJson
+from peds import open_ped
 
 
 # map the integer reported sex values to the enum
@@ -18,13 +20,24 @@ def cli_main():
     parser.add_argument('ped_file', help='path to the extended PED file')
     parser.add_argument('output', help='stem path to write phenopacket and new PED file')
     args = parser.parse_args()
+    main(args.ped_file, args.output)
 
-    extended_ped = open_ped(args.ped_file)
+
+def main(ped_file: str, output: str) -> None:
+    """
+    Create a Phenopacket from a pedigree file
+
+    Args:
+        ped_file (str): path to the extended PED file
+        output (str): stem path to write phenopacket and new PED file
+    """
+    extended_ped = open_ped(ped_file)
+    minimal_ped: list[str] = []
 
     # create the cohort shell
     cohort = pps2.Cohort(
         id='unknown',
-        description=f'Phenotypic data from {args.ped_file}',
+        description=f'Phenotypic data from {ped_file}',
         members=[],
         meta_data=pps2.MetaData(
             created_by='Talos',
@@ -41,29 +54,41 @@ def cli_main():
         ),
     )
 
-    for participant in extended_ped.members:
-        # todo save stripped back pedigree entry
-
-        # create a Phenopacket for this individual, and add it to the cohort
-        cohort.members.append(
-            pps2.Phenopacket(
-                # primary ID is the internal CPG ID
-                id=participant.id,
-                subject=pps2.Individual(
-                    # ID is the Family ID; Cohort model doesn't explicitly accommodate Family ID, but we need it
-                    id=participant.family,
-                    # alternate_ids captures the external ID, and is optional
-                    alternate_ids=[participant.data[0]] if participant.data else None,
-                    sex=reported_sex_map.get(participant.sex, pps2.Sex.UNKNOWN_SEX),
-                ),
-                # if HPO terms were provided, add them
-                phenotypic_features=[
-                    pps2.PhenotypicFeature(type=pps2.OntologyClass(id=hpo)) for hpo in participant.data[1:]
-                ],
+    for family in extended_ped:
+        for participant in family:
+            # create a Phenopacket for this individual, and add it to the cohort
+            cohort.members.append(
+                pps2.Phenopacket(
+                    # primary ID is the internal CPG ID
+                    id=participant.id,
+                    subject=pps2.Individual(
+                        # ID is the Family ID; Cohort model doesn't explicitly accommodate Family ID, but we need it
+                        id=participant.family,
+                        # alternate_ids captures the external ID, and is optional
+                        alternate_ids=[participant.data[0]] if participant.data else None,
+                        sex=reported_sex_map.get(participant.sex, pps2.Sex.UNKNOWN_SEX),
+                    ),
+                    # if HPO terms were provided, add them
+                    phenotypic_features=[
+                        pps2.PhenotypicFeature(type=pps2.OntologyClass(id=hpo)) for hpo in participant.data[1:]
+                    ],
+                )
             )
-        )
+            # save the minimal pedigree entry
+            participant.data = []
+            minimal_ped.append(str(participant))
 
-    # todo save both things
+    # make a parent directory for the output, if required
+    if not exists(output):
+        makedirs(output)
+
+    # save the phenopacket JSON
+    with open(join(output, 'phenopackets.json'), 'w', encoding='utf-8') as handle:
+        handle.write(MessageToJson(cohort))
+
+    # save the pedigree
+    with open(join(output, 'pedigree.ped'), 'w', encoding='utf-8') as handle:
+        handle.write(''.join(minimal_ped))
 
 
 if __name__ == '__main__':
