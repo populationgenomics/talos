35b2d1a993b2d502b8e56c43c80e20b23dc25443
MattWellie <mattwellie@gmail.com>
2025-03-24 15:44:34 +1000

    Bump version: 7.0.0 → 7.0.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     name='talos',
     description='Centre for Population Genomics Variant Prioritisation',
     long_description=readme,
-    version='7.0.0',
+    version='7.0.1',
     author='Matthew Welland, CPG',
     author_email='matthew.welland@populationgenomics.org.au, cas.simons@populationgenomics.org.au',
     package_data={'talos': ['templates/*.jinja', 'example_config.toml']},
