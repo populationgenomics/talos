a6c1ccf1b6309bc4f3a10a665a4879f810b6ea8d
Tobias Sargeant <toby.sargeant@populationgenomics.org.au>
2025-07-22 18:17:05 +1000

    Make large file downloader script more fancy.
    * run each curl in a tmux pane, if tmux is available.
    * correct the URL for the hg38 download.

--- large_files/gather_files.sh
+++ large_files/gather_files.sh
@@ -2,76 +2,117 @@
 # This runs a download from multiple different sources
 # Output file names created/expected by this script match the initial configuration file content in `talos.config` and `annotation.config`
 
+TMX=$(command -v tmux)
+POLL_INTERVAL=1
+SESSION_NAME="download_manager"
+WINDOW_NAME="Downloads"
+TMX_WINDOW_ID=""
+
+if [ ! -z "$TMX" ] && [ -z "$TMUX" ]; then
+  # tmux installed, but not in a tmux session. restart in tmux.
+  tmux new-session -d -s "$SESSION_NAME" sh "$0" "$@"
+  tmux attach-session -t "$SESSION_NAME"
+  exit 0
+fi
+
+cleanup() {
+  echo "[INFO] Caught exit signal. Cleaning up..."
+  CURRENT_PGID=$(ps -o pgid= -p $$ | tr -d '[:space:]')
+  pkill -SIGTERM -g "$CURRENT_PGID" -f curl
+
+  if [ -z $TMX ]; then
+	wait
+  else
+    # Kill the tmux window we created for the downloads
+    if $TMX list-windows | grep -q "$WINDOW_NAME"; then
+      $TMX kill-window -t "$TMX_WINDOW_ID"
+    fi
+  fi
+  echo "[INFO] Cleanup complete."
+  exit 0
+}
+
+start_download() {
+  local url="$1"
+  local output="$2"
+  local banner="$3"
+
+  if [ -z "$output" ]; then
+    output=$(basename "$url")
+  fi
+  if [ -z "$banner" ]; then
+    banner="Downloading [$output]"
+  fi
+  local cmd="echo \"$banner\"; echo; curl -C - -# -L --fail -o \"$output\" \"$url\";"
+  if [ -z $TMX ]; then
+     ( eval "$cmd" ) &
+  else
+    echo "[JOB START] Starting download for: $url to $output."
+    if [ -z "$TMX_WINDOW_ID" ]; then
+      TMX_WINDOW_ID=$($TMX new-window -P -n "$WINDOW_NAME" "$cmd")
+    else
+      $TMX split-window -v -t "$TMX_WINDOW_ID" "$cmd"
+      $TMX select-layout -t "$TMX_WINDOW_ID" even-vertical
+    fi
+  fi
+}
+
+await() {
+  if [ -z $TMX ]; then
+    wait
+  else
+  last_pane_count=-1
+    while true; do
+      pane_count=$($TMX list-panes -t "$TMX_WINDOW_ID" | wc -l)
+      if [ $last_pane_count != $pane_count ]; then
+        $TMX select-layout -t "$TMX_WINDOW_ID" even-vertical
+        last_pane_count=$pane_count
+      fi
+      if [ $pane_count == 0 ]; then
+        break;
+      fi
+      sleep "$POLL_INTERVAL"
+    done
+  fi
+}
+
+trap cleanup SIGINT SIGTERM
+
 # Echtvar-encoded gnomAD 4.1 population frequencies - this is a big one (~6GB) so it's started early and backgrounded
 ECHTVAR_FILE="gnomad_4.1_region_merged_GRCh38_whole_genome"
-if [ ! -f ${ECHTVAR_FILE} ]; then
-    echo "Downloading Echtvar from https://zenodo.org/records/15222100"
-    curl https://zenodo.org/records/15222100/files/gnomad_4.1_region_merged_GRCh38_whole_genome?download=1 -o "${ECHTVAR_FILE}" &
-else
-    echo "${ECHTVAR_FILE} already exists"
-fi
+start_download https://zenodo.org/records/15222100/files/gnomad_4.1_region_merged_GRCh38_whole_genome?download=1 "${ECHTVAR_FILE}" "\
+Downloading Echtvar from https://zenodo.org/records/15222100"
 
 # Monarch phenotype DB - another large download
 PHENIO_DB="phenio.db.gz"
-if [ ! -f ${PHENIO_DB} ]; then
-    curl https://data.monarchinitiative.org/monarch-kg/latest/phenio.db.gz -o "${PHENIO_DB}" &
-else
-    echo "${PHENIO_DB} already exists"
-fi
+start_download https://data.monarchinitiative.org/monarch-kg/latest/phenio.db.gz "${PHENIO_DB}"
 
 # GRCh38 reference genome
 GRCh38="ref.fa"
-if [ ! -f ${GRCh38} ]; then
-    echo "Downloading GRCh38 reference genome from the Broad Institute public data bucket"
-    echo "n.b. this is a GRCh38 reference genome, and your data must also be aligned to this reference genome for Talos to work correctly"
-    curl https://storage.cloud.google.com/gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta -o "${GRCh38}"
-else
-    echo "${GRCh38} already exists"
-fi
+GRCh38_URL="https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta?alt=media"
+start_download $GRCh38_URL ${GRCh38} "${MSG}" "\
+Downloading GRCh38 reference genome from the Broad Institute public data bucket
+n.b. this is a GRCh38 reference genome, and your data must also be aligned to this reference genome for Talos to work correctly"
 
 # MANE gene data
-MANE="MANE.GRCh38.v1.4.summary.txt.gz"
-if [ ! -f ${MANE} ]; then
-    curl https://ftp.ncbi.nlm.nih.gov/refseq/MANE/MANE_human/release_1.4/MANE.GRCh38.v1.4.summary.txt.gz -o "${MANE}"
-else
-    echo "${MANE} already exists"
-fi
+start_download https://ftp.ncbi.nlm.nih.gov/refseq/MANE/MANE_human/release_1.4/MANE.GRCh38.v1.4.summary.txt.gz
 
 # Ensembl GFF3 data
 GFF3="Homo_sapiens.GRCh38.113.gff3.gz"
-if [ ! -f ${GFF3} ]; then
-    curl https://ftp.ensembl.org/pub/release-113/gff3/homo_sapiens/Homo_sapiens.GRCh38.113.chr.gff3.gz -o "${GFF3}"
-else
-    echo "${GFF3} already exists"
-fi
+start_download https://ftp.ensembl.org/pub/release-113/gff3/homo_sapiens/Homo_sapiens.GRCh38.113.chr.gff3.gz
 
 # Jax lab file for phenotype matching
-OBO="hp.obo"
-if [ ! -f ${OBO} ]; then
-    curl https://github.com/obophenotype/human-phenotype-ontology/releases/download/v2025-05-06/hp.obo -o "${OBO}"
-else
-    echo "${OBO} already exists"
-fi
+start_download https://github.com/obophenotype/human-phenotype-ontology/releases/download/v2025-05-06/hp.obo
 
-G2P="genes_to_phenotype.txt"
-if [ ! -f ${G2P} ]; then
-    curl https://github.com/obophenotype/human-phenotype-ontology/releases/download/v2025-05-06/genes_to_phenotype.txt -o "${G2P}"
-else
-    echo "${G2P} already exists"
-fi
+start_download https://github.com/obophenotype/human-phenotype-ontology/releases/download/v2025-05-06/genes_to_phenotype.txt
 
 # latest clinvarbitration data
 CLINVAR="clinvarbitration.tar.gz"
-if [ ! -f ${CLINVAR} ]; then
-    curl https://zenodo.org/records/15896156/files/clinvarbitration_25-07_clinvar_decisions.release.tar.gz?download=1 -o "${CLINVAR}"
-else
-    echo "ClinvArbitration data already exists (${CLINVAR}), please check it's up to date (released monthly)"
-fi
+start_download https://zenodo.org/records/15896156/files/clinvarbitration_25-07_clinvar_decisions.release.tar.gz?download=1 "${CLINVAR}"
 
 # AlphaMissense raw data
 AM="AlphaMissense_hg38.tsv.gz"
-if [ ! -f ${AM} ]; then
-    curl https://zenodo.org/records/8208688/files/AlphaMissense_hg38.tsv.gz?download=1 -o "${AM}"
-else
-    echo "${AM} already exists"
-fi
+start_download "https://zenodo.org/records/8208688/files/AlphaMissense_hg38.tsv.gz?download=1" "${AM}"
+
+await
+
