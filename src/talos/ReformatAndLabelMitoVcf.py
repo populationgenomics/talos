be3913578f035da0ff1a5a19e45b860fe567047b
MattWellie <mattwellie@gmail.com>
2025-09-23 10:28:40 +1000

    delete one script, consolidate both into one behaviour

--- src/talos/ReformatAndLabelMitoVcf.py
+++ src/talos/ReformatAndLabelMitoVcf.py
@@ -7,9 +7,7 @@ re-arrange it into a HailTable for use with the Talos pipeline.
 This process combines the AF/CSQs already applied with the MANE transcript/protein names, and AlphaMissense annotations
 """
 
-import json
 from argparse import ArgumentParser
-from collections import defaultdict
 
 from cpg_utils.hail_batch import init_batch
 from loguru import logger
@@ -17,12 +15,20 @@ from loguru import logger
 import hail as hl
 
 from talos.annotation_scripts.ReformatAnnotatedVcfIntoHailTable import (
-    extract_and_split_csq_string,
-    csq_strings_into_hail_structs,
     MISSING_FLOAT,
     MISSING_INT,
     MISSING_STRING,
+    extract_and_split_csq_string,
 )
+from talos.models import PanelApp
+from talos.pedigree_parser import PedigreeParser
+from talos.RunHailFiltering import (
+    annotate_clinvarbitration,
+    filter_matrix_by_ac,
+    subselect_mt_to_pedigree,
+    csq_struct_to_string,
+)
+from talos.utils import read_json_from_path
 
 
 def csq_strings_into_hail_structs(csq_strings: list[str], mt: hl.MatrixTable) -> hl.MatrixTable:
@@ -56,7 +62,11 @@ def csq_strings_into_hail_structs(csq_strings: list[str], mt: hl.MatrixTable) ->
                 hl.if_else(
                     hl.len(x) == 6,
                     x.extend([MISSING_STRING]),
-                    x,
+                    hl.if_else(
+                        hl.len(x) == 3,
+                        x.extend([MISSING_STRING, MISSING_STRING, MISSING_STRING, MISSING_STRING]),
+                        x,
+                    ),
                 ),
             ),
         ),
@@ -88,6 +98,7 @@ def csq_strings_into_hail_structs(csq_strings: list[str], mt: hl.MatrixTable) ->
                 ),
                 am_class=MISSING_STRING,
                 am_pathogenicity=MISSING_FLOAT,
+                # MANE transcript selection has not been applied to mitochondrial data
                 mane_status=MISSING_STRING,
                 ensp=MISSING_STRING,
                 mane_id=MISSING_STRING,
@@ -99,19 +110,24 @@ def csq_strings_into_hail_structs(csq_strings: list[str], mt: hl.MatrixTable) ->
 
 def cli_main():
     """
-    take an input VCF and an output MT path
-    also supply the alpha_missense table created by parse_amissense_into_ht.py
+    take an input VCF and an output MT path, apply the correct reformatting/schema to the annotations
     """
 
     parser = ArgumentParser(description='Takes a BCSQ annotated VCF and makes it a mt')
     parser.add_argument('--input', help='Path to the annotated sites-only VCF', required=True)
     parser.add_argument('--output', help='output Table path, must have a ".mt" extension', required=True)
+    parser.add_argument('--panelapp', help='Path to a PanelApp data file for the cohort', required=True)
+    parser.add_argument('--pedigree', help='Path to the pedigree file for the cohort', required=True)
+    parser.add_argument('--clinvar', help='Path to a ClinvArbitration decisions HT', required=True)
     parser.add_argument('--batch', help='flag to use the batch hail backend', action='store_true')
     args = parser.parse_args()
 
     main(
         vcf_path=args.input,
         output_path=args.output,
+        panelapp=args.panelapp,
+        pedigree=args.pedigree,
+        clinvar_path=args.clinvar,
         batch=args.batch,
     )
 
@@ -119,16 +135,29 @@ def cli_main():
 def main(
     vcf_path: str,
     output_path: str,
+    panelapp: str,
+    pedigree: str,
+    clinvar_path: str,
     batch: bool,
 ):
     """
-    Takes a VEP-annotated Mito VCF, reorganises into a Talos-compatible MatrixTable
+    Takes a BCFtools-annotated Mito VCF, reorganises into a Talos-compatible MatrixTable
 
     Args:
         vcf_path (str): path to the annotated sites-only VCF
-        output_path (str): path to write the resulting Hail Table to, must
-        batch (bool): whether or not to batch the Hail Batch backend
+        output_path (str): path to write the resulting Hail Table to
+        panelapp (str): path to the panelapp data file
+        pedigree: path to the pedigree file
+        clinvar_path (str): path to the clinvar data file
+        batch (bool): if we should use the Hail Batch backend
     """
+
+    panel_data = read_json_from_path(panelapp, return_model=PanelApp)
+
+    # gets a lookup of all relevant genes from the PanelApp dump
+    symbol_to_ensg = {gene.symbol: key for key, gene in panel_data.genes.items() if gene.chrom.startswith('M')}
+    symbol_to_ensg_hl = hl.literal(symbol_to_ensg)
+    green_ensg_ids = hl.literal(set(symbol_to_ensg.values()))
 
     if batch:
         logger.info('Using Hail Batch backend')
@@ -143,17 +172,31 @@ def main(
     # read the VCF into a MatrixTable
     mt = hl.import_vcf(vcf_path, array_elements_required=False, force_bgz=True)
 
+    # remove filtered variants
+    mt = mt.filter_rows(hl.is_missing(mt.filters) | (mt.filters.length() == 0))
+
+    # parse the pedigree into an object
+    pedigree_data = PedigreeParser(pedigree)
+
+    # subset to currently considered samples
+    mt = subselect_mt_to_pedigree(mt, ped_samples=pedigree_data.get_all_sample_ids())
+
+    mt = annotate_clinvarbitration(mt=mt, clinvar=clinvar_path)
+
+    mt = filter_matrix_by_ac(mt)
+
     # re-shuffle the BCSQ elements
     mt = csq_strings_into_hail_structs(csq_fields, mt)
 
-    # todo no ENSG IDs currently available - investigate MANE MT genes
     mt = mt.annotate_rows(
+        # work backwards, use panelapp to get ENSGs. BCFtools isn't providing this, we get it from MANE (No MT MANE)
         transcript_consequences=hl.map(
             lambda x: x.annotate(
-                gene_id=x.gene,
+                gene_id=symbol_to_ensg_hl.get(x.gene, x.gene),
             ),
             mt.transcript_consequences,
         ),
+        # todo generate gnomAD Mito annotations from some source
         gnomad=hl.struct(
             gnomad_AC=MISSING_INT,
             gnomad_AF=MISSING_FLOAT,
@@ -168,9 +211,27 @@ def main(
     # drop the BCSQ field
     mt = mt.annotate_rows(info=mt.info.drop('BCSQ'))
 
-    mt.describe()
+    mt = mt.explode_rows(mt.gene_ids)
 
-    mt.write(output_path, overwrite=True)
+    # hard filter remaining rows to PanelApp green genes
+    mt = mt.filter_rows(green_ensg_ids.contains(mt.gene_ids))
+
+    # filter everything else out
+    mt = mt.filter_rows(mt.info.categorybooleanclinvarplp == 1)
+
+    # obtain the massive CSQ string using method stolen from the Broad's Gnomad library
+    # also take the single gene_id (from the exploded attribute)
+    # and retrieves the gnomAD annotations
+    mt = mt.annotate_rows(
+        info=mt.info.annotate(
+            **mt.gnomad,
+            csq=csq_struct_to_string(mt.transcript_consequences),
+            gene_id=mt.gene_ids,
+        ),
+    )
+
+    # now write that badboi
+    hl.export_vcf(mt, output_path, tabix=True)
 
 
 if __name__ == '__main__':
