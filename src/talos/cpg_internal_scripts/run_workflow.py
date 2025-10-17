c2344e67f5c46b716f9099776d87b2e60e2ace56
MattWellie <mattwellie@gmail.com>
2025-10-17 14:11:25 +1000

    upgrade to cpg-flow 1.0

--- src/talos/cpg_internal_scripts/run_workflow.py
+++ src/talos/cpg_internal_scripts/run_workflow.py
@@ -18,6 +18,7 @@ def cli_main():
 
 def main(dry_run: bool = False):
     workflow.run_workflow(
+        name='talos',
         stages=[talos_stages.MinimiseOutputForSeqr, talos_stages.UpdateIndexFile],
         dry_run=dry_run,
     )
