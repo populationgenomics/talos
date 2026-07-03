6674887987d29c9733cbaf41159ed2700d1710c6
MattWellie <mattwellie@gmal.com>
2026-07-03 14:30:52 +1000

    different solution, simpler

--- src/talos/cpg_internal_scripts/talos_stages.py
+++ src/talos/cpg_internal_scripts/talos_stages.py
@@ -743,7 +743,6 @@ class CreateTalosHtml(stage.CohortStage):
             hash_value=get_date_string(),
         )
         static_web_prefix = generate_dataset_prefix(
-            cohort=cohort,
             category='web',
             stage_name=self.name,
             hash_value='talos_static',
