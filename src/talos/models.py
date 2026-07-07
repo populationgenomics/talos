4123e395942b7907f4a4b14c9927d0671c3cfd88
MattWellie <mattwellie@gmail.com>
2026-07-07 14:03:20 +1000

    pre-commit update, and a little lint

--- src/talos/models.py
+++ src/talos/models.py
@@ -20,8 +20,8 @@ from talos.liftover.lift_2_0_0_to_2_1_0 import panelapp as pa_200_to_210
 from talos.liftover.lift_2_0_0_to_2_1_0 import resultdata as rd_200_to_210
 from talos.liftover.lift_2_1_0_to_2_2_0 import dl_panelapp as dl_pa_210_to_220
 from talos.liftover.lift_2_1_0_to_2_2_0 import resultdata as rd_210_to_220
-from talos.liftover.lift_2_2_0_to_2_3_0 import panelapp as pa_220_to_230
 from talos.liftover.lift_2_2_0_to_2_3_0 import dl_panelapp as dl_pa_220_to_230
+from talos.liftover.lift_2_2_0_to_2_3_0 import panelapp as pa_220_to_230
 from talos.liftover.lift_none_to_1_0_0 import resultdata as rd_none_to_1_0_0
 from talos.static_values import get_granular_date
 
