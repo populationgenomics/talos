4738d33334bb5a75c324a3155f5dc24d18d31371
MattWellie <mattwellie@gmail.com>
2026-09-22 18:48:59 +1000

    smaller model name

--- src/talos/models.py
+++ src/talos/models.py
@@ -472,7 +472,7 @@ class PanelApp(BaseModel):
     str_symbols: set[str] = Field(default_factory=set)
 
 
-class DownloadedPanelAppPanelDetail(BaseModel):
+class DownloadedPanelAppPanel(BaseModel):
     """ """
 
     moi: str
@@ -488,7 +488,7 @@ class DownloadedPanelAppGene(BaseModel):
     location: str = Field(default_factory=str)
     ensg: str = Field(default_factory=str)
     # for every panel this gene has featured in, when did it become Green, and what was the MOI
-    panels: dict[int, DownloadedPanelAppPanelDetail] = Field(default_factory=dict)
+    panels: dict[int, DownloadedPanelAppPanel] = Field(default_factory=dict)
 
 
 class DownloadedPanelAppStr(BaseModel):
@@ -500,7 +500,7 @@ class DownloadedPanelAppStr(BaseModel):
     chrom: str = Field(default_factory=str)
     location: str = Field(default_factory=str)
     # for every panel this gene has featured in, when did it become Green, and what was the MOI
-    panels: dict[int, DownloadedPanelAppPanelDetail] = Field(default_factory=dict)
+    panels: dict[int, DownloadedPanelAppPanel] = Field(default_factory=dict)
     normal_repeats: int = Field(default_factory=int)
     pathogenic_repeats: int = Field(default_factory=int)
 
